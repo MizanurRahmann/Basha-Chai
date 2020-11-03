e1c03f6d159b1479609c47b18a3a589587a3259b
MizanurRahmann <mr926560@gmail.com>
2020-11-03 22:03:14 +0600

    ADD: flat description right side.

--- src/components/Flat/Flat.js
+++ src/components/Flat/Flat.js
@@ -8,26 +8,29 @@ import interior4 from './../../resources/i-7.jpg';
 import bed from '../../resources/d-bed.svg';
 import bath from '../../resources/d-bath.svg';
 import space from '../../resources/d-space.svg';
+import owner from './../../resources/i-7.jpg';
+import { Link } from 'react-router-dom';
 
 function Flat() {
     return (
         <div className="flat__desc">
+            {/* ====== FLAT DESCRIPTION ====== */}
             <div className="flat__desc-desc">
                 {/* --- flat image --- */}
                 <div className="flat__desc-image">
-                    <img src={interior1} width="100%"/>
+                    <img src={interior1} width="100%" />
                     <div className="flat__desc-imageBox">
-                        <div className="image" style={{background: `url(${interior2}) no-repeat center center / cover`}}></div>
-                        <div className="image" style={{background: `url(${interior3}) no-repeat center center / cover`, margin: "0 20px"}}></div>
-                        <div className="image" style={{background: `url(${interior4}) no-repeat center center / cover`}}></div>
+                        <div className="image" style={{ background: `url(${interior2}) no-repeat center center / cover` }}></div>
+                        <div className="image" style={{ background: `url(${interior3}) no-repeat center center / cover`, margin: "0 20px" }}></div>
+                        <div className="image" style={{ background: `url(${interior4}) no-repeat center center / cover` }}></div>
                     </div>
                 </div>
                 {/* --- flat name and location --- */}
                 <div className="flat__desc-name">
                     <h1>
-                        The Grand Munir villa 
+                        The Grand Munir villa
                         <span>Khandar, Bogra</span>
-                        <span className="price">Starts form : <strong>12000 ৳</strong></span>
+                        <span className="price">Starts form : <strong>12,000 ৳</strong></span>
                     </h1>
                     <p>Beside Passport Office, Gohail road, Bogra 5800, Bogra</p>
                 </div>
@@ -47,7 +50,7 @@ function Flat() {
                             <div className="info-image"><img src={space} width="18px" alt="" /></div>
                             <div className="info-value"><strong>1260</strong> ft<sup>2</sup></div>
                         </div>
-                        
+
                     </div>
                 </div>
 
@@ -67,7 +70,117 @@ function Flat() {
                     <button className="add-to-fvrt"><i class="far fa-heart"></i> Add to favourite</button>
                 </div>
             </div>
-            <div className="flat__desc-owner"></div>
+
+            {/* ========= FLAT DESC OWNER ======== */}
+            <div className="flat__desc-owner">
+                <div className="owner-info">
+                    <div className="owner-image"><img src={owner} alt="" /></div>
+                    <div className="owner-name">
+                        <h2>Boshundhora group</h2>
+                        <p>Joined OCT 2020</p>
+                        <Link to="/">view profile</Link>
+                    </div>
+                </div>
+
+                {/* --- owner description --- */}
+                <div className="owner-description">
+                    <p>Vitae turpis at nibh beddictum dapibus. Donec vehicula condimentum felis, vel tincidunt arcu scelerisque quis. Etiam ac vestibulum leo. Aliquam augue tellus, vulputate et fringilla sit amet, vestibulum ut velit.</p>
+                </div>
+
+                {/* --- owner contact info --- */}
+                <div className="owner-contact">
+                    <div className="owner-contact-info">
+                        <div className="title">Language</div>
+                        <div className="value">English, Bangla</div>
+                    </div>
+                    <div className="owner-contact-info">
+                        <div className="title">Response rate</div>
+                        <div className="value">100%</div>
+                    </div>
+                    <div className="owner-contact-info">
+                        <div className="title">Response time</div>
+                        <div className="value">Within an hour</div>
+                    </div>
+                    <div className="owner-contact-info contactWith">
+                        <div className="title">Contact with</div>
+                        <div className="value">
+                            <div className="call"><i class="fas fa-phone"></i> Call</div>
+                            <div className="email"><i class="far fa-envelope"></i> Email</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="relatedFlats">
+                    <h2>Properties Nearby</h2>
+                    {/* ---- single flat --- */}
+                    <div className="relatedflat">
+                        <div className="flat-image">
+                            <img src={interior1} alt="" />
+                        </div>
+                        <div className="flat-desc">
+                            <h3>14,000 ৳</h3>
+                            <p>21 street, north kollanpara, Bogra</p>
+
+                            <div className="specs">
+                                <div className="spec"><img src={bed} alt="" width="16px" /> 4</div>
+                                <div className="spec"><img src={bath} alt="" width="16px" /> 2</div>
+                                <div className="spec"> <img src={space} alt="" width="16px" /> 1200 </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* ---- single flat --- */}
+                    <div className="relatedflat">
+                        <div className="flat-image">
+                            <img src={interior2} alt="" />
+                        </div>
+                        <div className="flat-desc">
+                            <h3>24,000 ৳</h3>
+                            <p>21 street, north kollanpara, Bogra</p>
+
+                            <div className="specs">
+                                <div className="spec"><img src={bed} alt="" width="16px" /> 5</div>
+                                <div className="spec"><img src={bath} alt="" width="16px" /> 3</div>
+                                <div className="spec"> <img src={space} alt="" width="16px" /> 2200 </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* ---- single flat --- */}
+                    <div className="relatedflat">
+                        <div className="flat-image">
+                            <img src={interior3} alt="" />
+                        </div>
+                        <div className="flat-desc">
+                            <h3>24,000 ৳</h3>
+                            <p>21 street, north kollanpara, Bogra</p>
+
+                            <div className="specs">
+                                <div className="spec"><img src={bed} alt="" width="16px" /> 5</div>
+                                <div className="spec"><img src={bath} alt="" width="16px" /> 3</div>
+                                <div className="spec"> <img src={space} alt="" width="16px" /> 2200 </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* ---- single flat --- */}
+                    <div className="relatedflat">
+                        <div className="flat-image">
+                            <img src={interior4} alt="" />
+                        </div>
+                        <div className="flat-desc">
+                            <h3>24,000 ৳</h3>
+                            <p>21 street, north kollanpara, Bogra</p>
+
+                            <div className="specs">
+                                <div className="spec"><img src={bed} alt="" width="16px" /> 5</div>
+                                <div className="spec"><img src={bath} alt="" width="16px" /> 3</div>
+                                <div className="spec"> <img src={space} alt="" width="16px" /> 2200 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
