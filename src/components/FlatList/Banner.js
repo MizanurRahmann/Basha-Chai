4efcff198a7de6480e738eced3c237848a4ef8b1
MizanurRahmann <mr926560@gmail.com>
2020-11-07 12:35:08 +0600

    ADD: carousel to banner.

--- src/components/FlatList/Banner.js
+++ src/components/FlatList/Banner.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import Slider from "react-slick";
 import bannerImage from './../../resources/banner-image.svg';
+import bannerImage2 from './../../resources/banner-image-2.svg';
 
 function Banner() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
     return (
-        <div className="banner">
-            <img src={bannerImage} alt="" height="100%"/>
-            <div className="text">
-                <h1><span>Get</span>15%</h1>
-                <h3>Black friday offer 🎉</h3>
+        <Slider {...settings} className="carousel">
+            <div className="banner">
+                <img src={bannerImage} alt="" height="100%" />
+                <div className="text">
+                    <h1><span>Get</span>15%<span> on rent</span></h1>
+                    <h3>Black friday offer 🎉</h3>
+                </div>
             </div>
-        </div>
+
+            <div className="banner">
+                <img src={bannerImage2} alt="" height="100%" />
+                <div className="text">
+                    <h1><span>Get</span>10%<span> on sell</span></h1>
+                    <h3>Black friday offer 🎉</h3>
+                </div>
+            </div>
+        </Slider>
     )
 }
 
